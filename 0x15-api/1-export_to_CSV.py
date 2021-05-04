1ca1d25884db66ce03d83a3695441f43939ecbfa
olmoshbtn <2120@holbertonschool.com>
2021-05-04 03:16:00 +0000

    chmod +x

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
-"""Script that, using this REST API, for a given employee ID,
- returns information about his/her TODO list progress"""
-import json
-import requests
-from sys import argv
+"""
+Summarize the TODO lists of all employees and write it to a file as JSON
+"""
+from json import dump
+from requests import get
 
+USERS = 'https://jsonplaceholder.typicode.com/users'
+TODOS = 'https://jsonplaceholder.typicode.com/todos'
 
 if __name__ == '__main__':
-    """Gets the name with the argument passed"""
-    #  if type(argv[1]) == int:
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(argv[1])).json()
-    EMPLOYEE_NAME = r.get('name')
-    """Gets tasks accomplished"""
-    t = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                     .format(argv[1])).json()
-    NUMBER_OF_DONE_TASKS = TASKS_NOT_DONE = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    TASK_TITLE = []
-    for key in t:
-        if key.get('completed') is True:
-            NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(key.get('title'))
-        elif key.get('completed') is False:
-            TASKS_NOT_DONE += 1
-        TOTAL_NUMBER_OF_TASKS = NUMBER_OF_DONE_TASKS + TASKS_NOT_DONE
-    print('Employee {} is done with tasks({}/{}):'
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for element in TASK_TITLE:
-        print("\t {}" .format(element))
+    with open('todo_all_employees.json', 'w') as ostream:
+        dump({
+            str(user['id']): [{
+                "username": user['username'],
+                "task": task['title'],
+                "completed": task['completed'],
+            } for task in get(TODOS, params={'userId': user['id']}).json()]
+            for user in get(USERS).json()
+        }, ostream)
