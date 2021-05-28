04863c9303799e589450f4116aabf0b99c149748
olmoshbtn <2120@holbertonschool.com>
2021-05-28 00:55:19 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increase file descriptor limit for NGINX
 
 exec { 'change':
-  command  => 'sudo sed -i "s/15/3000/" /etc/default/nginx',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
   provider => shell,
   before   => Exec['restart'],
 }
